fdea8232df14d462851b7eaa40dc34f604f33cf9
crw4147 <crw4147@rit.edu>
2018-10-19 00:49:54 -0400

    Basic DB Models Set Up

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,28 +1,54 @@
 // pull in our models. This will automatically load the index.js from that folder
 const models = require('../models');
 
+const Dog = models.Dog.DogModel;
+
 // default fake data so that we have something to work with until we make a real Cat
-const defaultData = {
-  name: 'unknown',
-  bedsOwned: 0,
-};
+// const defaultData = {
+//   name: 'unknown',
+//   breed: 'unknown',
+//   age: 0,
+// };
 
 let lastAdded;
 
 const hostIndex = (req, res) => {
-
+  res.render('index', {
+    currentName: lastAdded.name,
+    title: 'Home',
+    pageName: 'Home Page',
+  });
 };
 
-const readAllCats = (req, res, callback) => {
-
+const readAllDogs = (req, res, callback) => {
+  Dog.find(callback);
 };
 
-const readCat = (req, res) => {
- 
+const readDog = (req, res) => {
+  const name1 = req.query.name;
+
+  const callback = (err, doc) => {
+    if (err) {
+      return res.json({ err });
+    }
+
+    return res.json(doc);
+  };
+
+  Dog.findByName(name1, callback);
 };
 
 const hostPage1 = (req, res) => {
+  const callback = (err, docs) => {
+    if (err) {
+      return res.json({ err }); // if error, return it
+    }
 
+    // return success
+    return res.render('page1', { dogs: docs });
+  };
+
+  readAllDogs(req, res, callback);
 };
 
 const hostPage2 = (req, res) => {
@@ -34,24 +60,55 @@ const hostPage3 = (req, res) => {
 };
 
 const getName = (req, res) => {
-
+  res.json({ name: lastAdded.name });
 };
 
 const setName = (req, res) => {
-  if (!req.body.firstname || !req.body.lastname || !req.body.beds) {
-    return res.status(400).json({ error: 'firstname,lastname and beds are all required' });
+  if (!req.body.name || !req.body.breed || !req.body.age) {
+    return res.status(400).json({ error: 'name, breed, and age are all required' });
   }
-  
+
+  const dogData = {
+    name: req.body.name,
+    breed: req.body.breed,
+    age: req.body.age,
+  };
+
+  const newDog = new Dog(dogData);
+
+  const savePromise = newDog.save();
+
+  savePromise.then(() => {
+    lastAdded = newDog;
+    res.json({ name: lastAdded.name, breed: lastAdded.breed, age: lastAdded.age });
+  })
+  .catch(err => res.json({ err }));
+
+  return res;
 };
 
 const searchName = (req, res) => {
   if (!req.query.name) {
     return res.json({ error: 'Name is required to perform a search' });
   }
-};
 
-const updateLast = (req, res) => {
-	
+  return Dog.findByName(req.query.name, (err, doc) => {
+    if (err) {
+      return res.json({ err });
+    }
+
+    if (!doc) {
+      return res.json({ error: 'No dogs found' });
+    }
+
+    const tmp = doc;
+    tmp.age++;
+
+    const savePromise = tmp.save();
+
+    return savePromise.then(() => res.json({ name: tmp.name, breed: tmp.breed, age: tmp.age }))
+    .catch(er => res.json({ er }));
+  });
 };
 
 const notFound = (req, res) => {
@@ -65,10 +122,9 @@ module.exports = {
   page1: hostPage1,
   page2: hostPage2,
   page3: hostPage3,
-  readCat,
+  readDog,
   getName,
   setName,
-  updateLast,
   searchName,
   notFound,
 };
